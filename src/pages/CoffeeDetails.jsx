d903c0f2dfca9b22849501c1ab97254e041dd7f2
Monir <mrmonir0558@gmail.com>
2024-11-02 18:24:01 +0600

    Header and Footer layout design done

---
+++ src/pages/CoffeeDetails.jsx
@@ -0,0 +1,9 @@
+const CoffeeDetails = () => {
+    return (
+        <div>
+            <h2>Coffee details page</h2>
+        </div>
+    );
+};
+
+export default CoffeeDetails;
---
+++ src/pages/Coffees.jsx
@@ -0,0 +1,9 @@
+const Coffees = () => {
+    return (
+        <div>
+            <h2>Coffees page</h2>
+        </div>
+    );
+};
+
+export default Coffees;
